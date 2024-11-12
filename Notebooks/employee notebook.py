08b0f39fa72a76882f76b3bc45b3268954068938
satul257 <satul257>
2024-11-12 08:54:21 +0000

    vacumm_optimize_timetravel commands

--- Notebooks/employee notebook.py
+++ Notebooks/employee notebook.py
@@ -4,4 +4,29 @@
 
 # COMMAND ----------
 
-# MAGIC %fs head dbfs:/user/hive/warehouse/employee/_delta_log/00000000000000000002.json
+# MAGIC %fs ls 'dbfs:/user/hive/warehouse'
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- store version to version 3
+# MAGIC select * from employee@v3;
+# MAGIC -- anothery way we can write
+# MAGIC select * from employee version as of 3
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- Optimize command
+# MAGIC --optimize employee zorder by (id)
+# MAGIC -- try to remove unused files after optimize by default retention period is 7 days and we can not set it to 0 until manually setting changed 
+# MAGIC --Vacuum employee retain 0 hours
+# MAGIC
+# MAGIC -- manually disable retention period (not recommended for production)
+# MAGIC set spark.databrick.delta.retentiondurationcheck.enabled =  false;
+
+# COMMAND ----------
+
+# MAGIC %fs ls 
